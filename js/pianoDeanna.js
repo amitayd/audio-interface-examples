a72f148321ee3cece1b1797297f604968fcb7b3f
shlomihod <hod_s@bezeqint.net>
2015-07-20 16:13:17 +0300

    Adding more dillemas to pianoDeanna.js

--- js/pianoDeanna.js
+++ js/pianoDeanna.js
@@ -9,6 +9,8 @@ var keyboard = createKeyboard({ width: '100%',
 Dilemma:
 Using new or create function?
 I guess it better if it would be with the same as creating widget
+I dont know now what is better "new" or function.
+In this file I choosed create, in another one I'll try with new, just to get the feeling
 */
 var synth = createSynth(); 
 
@@ -25,6 +27,10 @@ keyboard.onPress(function(note) {
   Which kind of timing? For the piano I think for sure seconds, it's the most intuitive one
   And there is not so much need for notes
   (It's more relevant to the sequencing and sampling)
+
+  Amitay: Maybe a solution would be the "Frequency Type" of Tone.js, which uses either frequency in hz,
+  and note names as note+Ocatave strings: 'a4', 'c5', etc
+  http://tonejs.org/docs/#Frequency
   */
   synth.triggerAttack(frequency, 0.25);
 })
@@ -33,5 +39,10 @@ keyboard.onRelease(function(note) {
   synth.triggerRelease();
 })
 
-
+/*
+Dilemma:
+Because we are not going to create crazy synth,
+maybe by default (as in gibber.js), every new instrument is connected
+to the master
+*/
 synth.toMaster();
